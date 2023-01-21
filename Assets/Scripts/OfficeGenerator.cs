2803995bb0892e762912dd0185587259fc71f739
realstone513 <ting6247@nate.com>
2023-01-21 16:02:27 +0900

    wall instantiate

--- Assets/Scripts/OfficeGenerator.cs
+++ Assets/Scripts/OfficeGenerator.cs
@@ -8,8 +8,44 @@ public class OfficeGenerator : MonoBehaviour
     public GameObject wallCorner;
     public GameObject wallDoor;
 
-    public void GenerateWall()
+    public void GenerateWall(Vector3 startPos, int row, int[] data)
     {
+        Vector3 interval = Vector3.zero;
+        Quaternion qi = Quaternion.identity;
+        int col = data.Length / row;
 
+        for (int c = 0; c < col; c++)
+        {
+            for (int r = 0; r < row; r++)
+            {
+                if (data[c * row + r] == 1)
+                    Instantiate(wallStandard, startPos + interval, qi);
+                interval.x += 1;
+            }
+            interval.x = startPos.x;
+            interval.z -= 1;
+        }
+    }
+
+    private void Start()
+    {
+        int row = 13;
+        int[] datas =
+        {
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1,
+            1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1,
+            1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1,
+            1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        };
+
+        GenerateWall(Vector3.zero, row, datas);
     }
 }
